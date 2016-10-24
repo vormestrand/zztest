0bfc3897de1bba11877f88034fe56a0e252ce2ca
Ingve Vormestrand <ingve@vormestrand.com>
2016-10-24 14:15:18 +0200

    Travis test

---
+++ test/uri_test.cpp
@@ -0,0 +1,20 @@
+#include <uri>
+#include <lest.hpp>
+
+/*
+const lest::test specification[] =
+{
+    CASE( "Empty string has length zero (succeed)" )
+    {
+        EXPECT( 0 == string(  ).length() );
+        EXPECT( 0 == string("").length() );
+    }
+}
+
+*/
+
+int main() {
+	uri::URI uri;
+	return 0;
+}
+
